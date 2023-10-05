2632f083b2e8449519c7c05af062e8f7c8e13264
Streetsize <dromeroanglat@gmail.com>
2023-10-04 23:40:54 -0300

    main

--- assets/js/pedido.js
+++ assets/js/pedido.js
@@ -1,3 +1,62 @@
+    // Asegurarse de que el código se ejecute después de que el DOM esté listo
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtener referencia al elemento de imagen de la primera prenda
+        const imagenRemera = document.getElementById('imagenRemera');
+        // Obtener referencia al elemento de imagen de la segunda prenda
+        const imagenRemera2 = document.getElementById('imagenRemera2');
+
+        // Obtener referencia al elemento de selección de color de la primera prenda
+        const selectColor = document.getElementById('color');
+        // Obtener referencia al elemento de selección de color de la segunda prenda
+        const selectColor2 = document.getElementById('color2');
+
+        // Definir un objeto que mapea los colores a las rutas de las imágenes para la primera prenda
+        const colorImagenes = {
+            Blanco: 'assets/img/remera/blanco.png',
+            Negro: 'assets/img/remera/negro.png',
+			AzulPetroleo: 'assets/img/remera/azulpetroleo.png',
+			Mostaza: 'assets/img/remera/mostaza.png',
+			Celeste: 'assets/img/remera/celeste.png',
+			Bordo: 'assets/img/remera/bordo.png',
+			Rosado: 'assets/img/remera/rosado.png',
+            // Agrega más rutas de imágenes para otros colores aquí
+        };
+
+        // Definir un objeto que mapea los colores a las rutas de las imágenes para la segunda prenda
+        const colorImagenes2 = {
+            Blanco: 'assets/img/remera/blanco.png',
+            Negro: 'assets/img/remera/negro.png',
+			AzulPetroleo: 'assets/img/remera/azulpetroleo.png',
+			Mostaza: 'assets/img/remera/mostaza.png',
+			Celeste: 'assets/img/remera/celeste.png',
+			Bordo: 'assets/img/remera/bordo.png',
+			Rosado: 'assets/img/remera/rosado.png',
+            // Agrega más rutas de imágenes para otros colores aquí
+        };
+
+        // Agregar un evento para detectar cambios en la selección de color de la primera prenda
+        selectColor.addEventListener('change', function() {
+            const selectedColor = this.value; // Obtener el valor seleccionado
+
+            // Verificar si hay una ruta de imagen para el color seleccionado en la primera prenda
+            if (colorImagenes[selectedColor]) {
+                // Actualizar la fuente de la imagen de la primera prenda con la ruta correspondiente
+                imagenRemera.src = colorImagenes[selectedColor];
+            }
+        });
+
+        // Agregar un evento para detectar cambios en la selección de color de la segunda prenda
+        selectColor2.addEventListener('change', function() {
+            const selectedColor = this.value; // Obtener el valor seleccionado
+
+            // Verificar si hay una ruta de imagen para el color seleccionado en la segunda prenda
+            if (colorImagenes2[selectedColor]) {
+                // Actualizar la fuente de la imagen de la segunda prenda con la ruta correspondiente
+                imagenRemera2.src = colorImagenes2[selectedColor];
+            }
+        });
+
+        // Agregar evento al checkbox "Agregar otra prenda" para mostrar u ocultar la segunda prenda
         document.getElementById('agregarOtraPrenda').addEventListener('change', function() {
             const segundaPrenda = document.getElementById('segundaPrenda');
 
@@ -8,6 +67,7 @@
             }
         });
 
+        // Agregar evento al checkbox "Envío a domicilio" para mostrar u ocultar la ubicación y el costo de envío
         document.getElementById('envioDomicilio').addEventListener('change', function() {
             const ubicacionDiv = document.getElementById('ubicacionDiv');
             const costoEnvio = document.getElementById('costoEnvio');
@@ -21,26 +81,27 @@
             }
         });
 
+        // Agregar evento al botón "Continuar" para generar el enlace de WhatsApp con los detalles del pedido
         document.getElementById('generar-enlace').addEventListener('click', function() {
             const talle1 = document.getElementById('talle').value;
             const color1 = document.getElementById('color').value;
-			const cantidad1 = document.getElementById('cantidad').value;
+            const cantidad1 = document.getElementById('cantidad').value;
             const envioDomicilio = document.getElementById('envioDomicilio').checked;
             const ubicacion = document.getElementById('ubicacion').value;
 
-            let mensaje = `¡Hola! Quiero ordenar *${cantidad1} remera Oversize 100% algodón con Talle: ${talle1} y Color: ${color1}.*`;
+            let mensaje = `¡Hola! Quiero ordenar ${cantidad1} remera Oversize 100% algodón con Talle: ${talle1} y Color: ${color1}.`;
 
             // Si el usuario ha marcado "Agregar otra prenda", agregamos los detalles de la segunda prenda al mensaje
             if (document.getElementById('agregarOtraPrenda').checked) {
                 const talle2 = document.getElementById('talle2').value;
                 const color2 = document.getElementById('color2').value;
-			const cantidad2 = document.getElementById('cantidad2').value;
+                const cantidad2 = document.getElementById('cantidad2').value;
 
-                mensaje += ` También quiero *${cantidad2} remera de Talle: ${talle2} y Color: ${color2}.*`;
+                mensaje += ` También quiero ${cantidad2} remera de Talle: ${talle2} y Color: ${color2}.`;
             }
-			
-			            if (envioDomicilio) {
-                mensaje += ` Mi ubicación de entrega es: *${ubicacion}*.`;
+
+            if (envioDomicilio) {
+                mensaje += ` Mi ubicación de entrega es: ${ubicacion}.`;
             } else {
                 mensaje += ' Deseo retirar mi producto en un punto de encuentro.';
             }
@@ -49,3 +110,4 @@
             const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=5492612527052&text=${mensajeCodificado}`;
             window.open(enlaceWhatsApp, '_blank');
         });
+    });
