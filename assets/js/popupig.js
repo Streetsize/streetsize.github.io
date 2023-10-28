9918400c15b870e84ecc1061a9ed33b2e1d63147
Streetsize <dromeroanglat@gmail.com>
2023-10-28 00:12:30 -0300

    main

--- assets/js/popupig.js
+++ assets/js/popupig.js
@@ -1,25 +1,32 @@
-// Función para mostrar el popup después de 30 segundos
+// Función para mostrar el popup solo la primera vez
 function mostrarPopup() {
-    setTimeout(function() {
-        var popupig = document.getElementById("popupig");
-        popupig.style.display = "block";
-    }, 15000); // 30 segundos en milisegundos
+    // Verificar si se ha establecido una cookie que indica que el popup ya se ha mostrado
+    if (document.cookie.indexOf("popupShown=true") === -1) {
+        // Establecer un temporizador de 15 segundos antes de mostrar el popup
+        setTimeout(function() {
+            var popupig = document.getElementById("popupig");
+            popupig.style.display = "block";
+        
+            // Habilitar la función de cierre después de 3 segundos
+            setTimeout(function() {
+                document.body.addEventListener("click", cerrarPopupig);
+            }, 3000); // 3 segundos en milisegundos
+        
+            // Establecer una cookie que indica que el popup se ha mostrado
+            document.cookie = "popupShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+        }, 15000); // 15 segundos en milisegundos
+    }
 }
 
 // Función para cerrar el popup
 function cerrarPopupig() {
     var popupig = document.getElementById("popupig");
     popupig.style.display = "none";
+    // Deshabilitar la función de cierre
+    document.body.removeEventListener("click", cerrarPopupig);
 }
 
-// Mostrar el popup después de 30 segundos al cargar la página
+// Llamar a la función mostrarPopup después de que se haya cargado la página
 window.addEventListener("load", mostrarPopup);
-
-// Cerrar el popup al hacer clic en cualquier parte de la página
-document.body.addEventListener("click", function(event) {
-    if (event.target === popupig) {
-        cerrarPopupig();
-    }
-});
 
 document.getElementById("close-popupig").addEventListener("click", cerrarPopupig);
